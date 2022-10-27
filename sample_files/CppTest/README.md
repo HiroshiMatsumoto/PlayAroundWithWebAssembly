5d4d5d1323df7bf6dcf4aec88df68bf36a9cd9f9
matsumoto <h_matsumoto@hotmail.com>
2022-10-27 23:14:34 +0900

    solve #1

---
+++ sample_files/CppTest/README.md
@@ -0,0 +1,7 @@
+# Transpile from C++ file
+
+use `em++` command
+
+```
+em++ test.cpp -o test.html
+```
---
+++ sample_files/CppTest/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "emscripten.h"
+
+
+
+int main(){
+  std::cout << "test" << std::endl;
+  return 0;
+}
